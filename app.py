d7ae46345c422c181e5568ac358c24deac2a61fc
OSSahota <osstechunlimited@gmail.com>
2019-02-21 17:00:29 +0000

    Added a set of questions to populate db table

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import os
 import config
 from flask import Flask, render_template, request
 from lib import generator
-
+from sqlalchemy.event import listen
+from sqlalchemy import event, DDL
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -11,7 +12,14 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from lib import questions
+from models import *
+
+@app.before_first_request
+def setup():
+    db.session.add(Question(question=u'q1 question1', choice1=u'q1 choice1', choice2=u'q1 choice2', choice3=u'q1 choice3', choice4=u'q1 choice4', answer=u'q1 choice1'))
+    db.session.add(Question(question=u'q2 question1', choice1=u'q2 choice1', choice2=u'q2 choice2', choice3=u'q2 choice3', choice4=u'q2 choice4', answer=u'q2 choice2'))
+    db.session.add(Question(question=u'q3 question1', choice1=u'q3 choice1', choice2=u'q3 choice2', choice3=u'q3 choice3', choice4=u'q3 choice4', answer=u'q3 choice3'))
+    db.session.commit()
 
 @app.route("/")
 def root():
@@ -40,7 +48,7 @@ def get_question():
             # print("Wrong! Try Again")
             return "Wrong! Try Again"
 
-    question = questions.generate_question()
+    # question = questions.generate_question()
     return render_template('question.html',question=question)
 
 if __name__ == '__main__':
