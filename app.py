5ce3043c49327158e908c47410f4980d63cf5eb6
Emanuele <emanuelegorga@gmail.com>
2019-02-23 14:14:35 +0000

    Added questions for part 1 ruby

--- app.py
+++ app.py
@@ -20,13 +20,18 @@ def setup():
     db.session.query(Question).delete()
     db.session.execute("ALTER SEQUENCE questions_id_seq RESTART WITH 1;")
     db.session.commit()
-    db.session.add(Question(question=u'q1 question', choice1=u'q1 choice1', choice2=u'q1 choice2', choice3=u'q1 choice3', choice4=u'q1 choice4', answer=u'q1 choice1'))
-    db.session.add(Question(question=u'q2 question', choice1=u'q2 choice1', choice2=u'q2 choice2', choice3=u'q2 choice3', choice4=u'q2 choice4', answer=u'q2 choice2'))
-    db.session.add(Question(question=u'q3 question', choice1=u'q3 choice1', choice2=u'q3 choice2', choice3=u'q3 choice3', choice4=u'q3 choice4', answer=u'q3 choice3'))
-    db.session.commit()
-    db.session.execute("INSERT INTO questions(question, choice1, choice2, choice3, choice4, answer) \
-                        VALUES ('q4 question', 'q4 choice1', 'q4 choice2', 'q4 choice3', 'q4 choice4', 'q4 choice4');"
-                        )
+# ------------ RUBY QUESTIONS PART 1 ------------
+    db.session.add(Question(question=u'1) How many data types are available in Ruby?', choice1=u'1', choice2=u'2', choice3=u'3', choice4=u'4', answer=u'3'))
+    db.session.add(Question(question=u'2) Which of the following is a correct way to define a string?', choice1=u'"Hello"', choice2=u'Hello', choice3=u'-Hello-', choice4=u'(Hello)', answer=u'"Hello"'))
+    db.session.add(Question(question=u'3) Which of the following is known as a boolean value?', choice1=u'Yes', choice2=u'Truth', choice3=u'Real', choice4=u'True', answer=u'True'))
+    db.session.add(Question(question=u'4) How do you display "Hi there!" on the screen?', choice1=u'put "Hi there!"', choice2=u'puts "Hi there!"', choice3=u'"puts Hi there!"', choice4=u'puts Hi there!', answer=u'puts "Hi there!"'))
+    db.session.add(Question(question=u'5) How do you display the number 23 on the screen?', choice1=u'23', choice2=u'puts 23', choice3=u'23 puts', choice4=u'puts "23"', answer=u'puts 23'))
+    db.session.add(Question(question=u'6) Which of the following is a correct way to assign the number 30 to the variable a?', choice1=u'a = 30', choice2=u'a(30)', choice3=u'30 = a', choice4=u'a == 30', answer=u'a = 30'))
+    db.session.add(Question(question=u'7) Given a variable a = 25, how do you display the value of the variable "a"?', choice1=u'puts "a"', choice2=u'puts variable(a)', choice3=u'puts a', choice4=u'a', answer=u'puts a'))
+    db.session.add(Question(question=u'8) Which of the following is not a valid datatype in Ruby?', choice1=u'Integer', choice2=u'String', choice3=u'Timedate', choice4=u'Boolean', answer=u'Timedate'))
+    db.session.add(Question(question=u'9) What is the extension used for saving the ruby file?', choice1=u'.ruby', choice2=u'.r', choice3=u'.rb', choice4=u'.ry', answer=u'.rb'))
+    db.session.add(Question(question=u'10) How do you comment out a single line of code in Ruby?', choice1=u'#', choice2=u'begin and end', choice3=u'//', choice4=u'<!- ->', answer=u'#'))
+# ------------ RUBY QUESTIONS PART 1 ------------
     db.session.commit()
 
 @app.route("/")
@@ -49,17 +54,14 @@ def get_all():
 
 @app.route("/tutorial_ruby")
 def tutorial_ruby():
-    # return "Welcome to our landing page :-)."
     return render_template('tutorialruby.html')
 
 @app.route("/tutorial_python")
 def tutorial_python():
-    # return "Welcome to our landing page :-)."
     return render_template('tutorialpython.html')
 
 @app.route("/tutorial_javascript")
 def tutorial_javascript():
-    # return "Welcome to our landing page :-)."
     return render_template('tutorialjavascript.html')
 
 @app.route("/question_ruby/<id_>", methods=['GET', 'POST'])
@@ -68,17 +70,17 @@ def get_question_by_id(id_):
     question = Question.query.filter_by(id=id_).first()
 
     if request.method =='POST':
-        print(request.form['question'])
+        # print(request.form['question'])
         if request.form['question'] == question.answer:
-            print("Correct Well done")
             with counter.get_lock():
                 counter.value += 1
                 id = counter.value
             button = Markup(f'<form method="GET" action="/question_ruby/{id}"><button type="submit">next</button></form>')
+            flash("Well done!")
             flash(button)
             return render_template('question.html',question=question)
         else:
-            flash('this is a test flash message')
+            flash('That is wrong. Try again!')
             return render_template('question.html',question=question)
 
     return render_template('question.html',question=question)
