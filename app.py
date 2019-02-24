c8914b5b4c6dd436fc421a7ace6804c36d75a063
Emanuele <emanuelegorga@gmail.com>
2019-02-24 17:18:35 +0000

    background image

--- app.py
+++ app.py
@@ -16,34 +16,6 @@ db = SQLAlchemy(app)
 
 from models import *
 
-@app.before_first_request
-def setup():
-    db.session.query(Question).delete()
-    db.session.execute("ALTER SEQUENCE questions_id_seq RESTART WITH 1;")
-    db.session.commit()
-# ------------ RUBY QUESTIONS PART 1 ------------
-    db.session.add(Question(question=u'1) How many data types are available in Ruby?', choice1=u'1', choice2=u'2', choice3=u'3', choice4=u'4', answer=u'3'))
-    db.session.add(Question(question=u'2) Which of the following is a correct way to define a string?', choice1=u'"Hello"', choice2=u'Hello', choice3=u'-Hello-', choice4=u'(Hello)', answer=u'"Hello"'))
-    db.session.add(Question(question=u'3) Which of the following is known as a boolean value?', choice1=u'Yes', choice2=u'Truth', choice3=u'Real', choice4=u'True', answer=u'True'))
-    db.session.add(Question(question=u'4) How do you display "Hi there!" on the screen?', choice1=u'put "Hi there!"', choice2=u'puts "Hi there!"', choice3=u'"puts Hi there!"', choice4=u'puts Hi there!', answer=u'puts "Hi there!"'))
-    db.session.add(Question(question=u'5) How do you display the number 23 on the screen?', choice1=u'23', choice2=u'puts 23', choice3=u'23 puts', choice4=u'puts "23"', answer=u'puts 23'))
-    db.session.add(Question(question=u'6) Which of the following is a correct way to assign the number 30 to the variable a?', choice1=u'a = 30', choice2=u'a(30)', choice3=u'30 = a', choice4=u'a == 30', answer=u'a = 30'))
-    db.session.add(Question(question=u'7) Given a variable a = 25, how do you display the value of the variable "a"?', choice1=u'puts "a"', choice2=u'puts variable(a)', choice3=u'puts a', choice4=u'a', answer=u'puts a'))
-    db.session.add(Question(question=u'8) Which of the following is not a valid datatype in Ruby?', choice1=u'Integer', choice2=u'String', choice3=u'Timedate', choice4=u'Boolean', answer=u'Timedate'))
-    db.session.add(Question(question=u'9) What is the extension used for saving the ruby file?', choice1=u'.ruby', choice2=u'.r', choice3=u'.rb', choice4=u'.ry', answer=u'.rb'))
-    db.session.add(Question(question=u'10) How do you comment out a single line of code in Ruby?', choice1=u'#', choice2=u'begin and end', choice3=u'//', choice4=u'<!- ->', answer=u'#'))
-# ------------ RUBY QUESTIONS PART 1 ------------
-    db.session.commit()
-
-    db.session.query(QuestionLevel2).delete()
-    db.session.execute("ALTER SEQUENCE questions_level2_id_seq RESTART WITH 1;")
-    db.session.commit()
-    db.session.add(QuestionLevel2(language=u'Ruby', question_display_id=1, question=u'q1 Which bootcamp are you currently studying at?', answer=u'Makers Academy'))
-    db.session.add(QuestionLevel2(language=u'Ruby', question_display_id=2, question=u'q2 Name of your cohort?', answer=u'November 2018'))
-    db.session.add(QuestionLevel2(language=u'Python', question_display_id=1, question=u'q1 What is a Python?', answer=u'Snake'))
-    db.session.commit()
-
-
 @app.route("/")
 def root():
     return render_template('index.html')
@@ -111,6 +83,34 @@ def get_questionlevel2_by_id(id_):
             return render_template('question2.html',question2=question2)
 
     return render_template('question2.html',question2=question2)
+
+@app.before_first_request
+def setup():
+    db.session.query(Question).delete()
+    db.session.execute("ALTER SEQUENCE questions_id_seq RESTART WITH 1;")
+    db.session.commit()
+# ------------ RUBY QUESTIONS PART 1 ------------
+    db.session.add(Question(question=u'1) How many data types are available in Ruby?', choice1=u'1', choice2=u'2', choice3=u'3', choice4=u'4', answer=u'3'))
+    db.session.add(Question(question=u'2) Which of the following is a correct way to define a string?', choice1=u'"Hello"', choice2=u'Hello', choice3=u'-Hello-', choice4=u'(Hello)', answer=u'"Hello"'))
+    db.session.add(Question(question=u'3) Which of the following is known as a boolean value?', choice1=u'Yes', choice2=u'Truth', choice3=u'Real', choice4=u'True', answer=u'True'))
+    db.session.add(Question(question=u'4) How do you display "Hi there!" on the screen?', choice1=u'put "Hi there!"', choice2=u'puts "Hi there!"', choice3=u'"puts Hi there!"', choice4=u'puts Hi there!', answer=u'puts "Hi there!"'))
+    db.session.add(Question(question=u'5) How do you display the number 23 on the screen?', choice1=u'23', choice2=u'puts 23', choice3=u'23 puts', choice4=u'puts "23"', answer=u'puts 23'))
+    db.session.add(Question(question=u'6) Which of the following is a correct way to assign the number 30 to the variable a?', choice1=u'a = 30', choice2=u'a(30)', choice3=u'30 = a', choice4=u'a == 30', answer=u'a = 30'))
+    db.session.add(Question(question=u'7) Given a variable a = 25, how do you display the value of the variable "a"?', choice1=u'puts "a"', choice2=u'puts variable(a)', choice3=u'puts a', choice4=u'a', answer=u'puts a'))
+    db.session.add(Question(question=u'8) Which of the following is not a valid datatype in Ruby?', choice1=u'Integer', choice2=u'String', choice3=u'Timedate', choice4=u'Boolean', answer=u'Timedate'))
+    db.session.add(Question(question=u'9) What is the extension used for saving the ruby file?', choice1=u'.ruby', choice2=u'.r', choice3=u'.rb', choice4=u'.ry', answer=u'.rb'))
+    db.session.add(Question(question=u'10) How do you comment out a single line of code in Ruby?', choice1=u'#', choice2=u'begin and end', choice3=u'//', choice4=u'<!- ->', answer=u'#'))
+# ------------ RUBY QUESTIONS PART 1 ------------
+    db.session.commit()
+
+    db.session.query(QuestionLevel2).delete()
+    db.session.execute("ALTER SEQUENCE questions_level2_id_seq RESTART WITH 1;")
+    db.session.commit()
+    db.session.add(QuestionLevel2(language=u'Ruby', question_display_id=1, question=u'q1 Which bootcamp are you currently studying at?', answer=u'Makers Academy'))
+    db.session.add(QuestionLevel2(language=u'Ruby', question_display_id=2, question=u'q2 Name of your cohort?', answer=u'November 2018'))
+    db.session.add(QuestionLevel2(language=u'Python', question_display_id=1, question=u'q1 What is a Python?', answer=u'Snake'))
+    db.session.commit()
+
 
 if __name__ == '__main__':
     app.run()
--- tests/test_front_end.py
+++ tests/test_front_end.py
@@ -1,13 +1,17 @@
 # import unittest
-# import urllib2
-#
-# from flask_testing import LiveServerTestCase
+# # from flask_testing import LiveServerTestCase
 # from selenium import webdriver
+# from flask import url_for
 #
 # # from app import create_app, db
 # from models import Question
 #
+# def setUp(self):
+#     # self.driver.find_element_by_id("introductioncoding").click()
+#     # print(driver.current_url)
+#     self.driver.find_element_by_id("introductioncoding").click()
 #
-#
+#     assert url_for('/introduction') in self.driver.current_url
+
 # if __name__ == '__main__':
 #     unittest.main()
