f3f7d1585148d27cbd87ecda776ae38fad880a29
celine kaslin <celine.kaslin@gmail.com>
2019-02-27 17:32:45 +0000

    chrome driver test

---
+++ tests/features/test_user_interaction.py
@@ -0,0 +1,5 @@
+from selenium .webdriver import Chrome
+
+path="c:\\celinekaslin\\Downloads\\chromedriver_mac64.zip\\chromedriver"
+driver = Chrome(executable_path=path)
+driver.get("http://localhost:5000/introduction")
